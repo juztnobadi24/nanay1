342ec2ecb64b9cb4ca2e511303a60934c0f6e71b
juztnobadi24 <137787521+juztnobadi24@users.noreply.github.com>
2026-04-01 06:01:08 +0800

    Update player.js

--- js/player.js
+++ js/player.js
@@ -111,7 +111,7 @@ class PlayerComponent {
             {
                 image: "https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2019/01/Aniplus_Asia_Winter_2019_Simulcast.png",
                 title: "AniPlus",
-                channelName: "AniPlus 2"
+                channelName: "AniPlus"
             },
             {
                 image: "https://library.sportingnews.com/styles/twitter_card_120x120/s3/2023-10/GFX-1143%20NBA%20TV%20schedule%20FTR.jpg?itok=B1xtr2Cl",
